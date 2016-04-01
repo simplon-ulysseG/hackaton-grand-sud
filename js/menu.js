64274ed4da82105b96d153e19412a78cf605814c
FlyingSocket <thomasmejovsek@hotmail.com>
2016-04-01 19:06:26 +0200

    version pre-finale du menu

--- js/menu.js
+++ js/menu.js
@@ -3,17 +3,17 @@ $( document ).ready(function() {
 
   //POUR LE RESPONSIVE
   if ($(window).width() <= 600) {
+    $('.titreAcc').addClass('bounceInLeft');
     //quand on clique sur le logo
     $('#logo').on('click', function() {
       $('.acc').css('height', '100%');
       //permet l'animation de retour
       $('.titreAcc').removeClass('bounceOutUp');
-      $('.titreAcc').addClass('bounceInDown');
+      $('.titreAcc').addClass('bounceInLeft');
       //on refixe les delay d'animation
       $('#acc1 .titreAcc').css('animation-delay', '0s');
       $('#acc2 .titreAcc').css('animation-delay', '.3s');
       $('#acc3 .titreAcc').css('animation-delay', '.6s');
-      // $('.titreAcc').show();
     })
 
     //Quand on clique sur un des pan de l'accordeon
@@ -21,7 +21,7 @@ $( document ).ready(function() {
         $('.acc').css('height', '0%'); //On ferme tout
         $(this).css('height', '100%'); //sauf celui qu'on selectionne
 
-        $('.titreAcc').removeClass('bounceInDown');
+        $('.titreAcc').removeClass('bounceInLeft');
         $('.titreAcc').css('animation-delay', '1s');
         $('.titreAcc').addClass('bounceOutUp');
       })
@@ -30,6 +30,7 @@ $( document ).ready(function() {
 
 
   else {
+    $('.titreAcc').addClass('bounceInDown');
   //quand on clique sur le logo
   $('#logo').on('click', function() {
     $('.acc').css('width', '100%');
@@ -40,7 +41,6 @@ $( document ).ready(function() {
     $('#acc1 .titreAcc').css('animation-delay', '0s');
     $('#acc2 .titreAcc').css('animation-delay', '.3s');
     $('#acc3 .titreAcc').css('animation-delay', '.6s');
-    // $('.titreAcc').show();
   })
 
   //Quand on clique sur un des pan de l'accordeon
