7d2b3adca122358e7033a75d2d5fe5f8450e4b23
FlyingSocket <thomasmejovsek@hotmail.com>
2016-04-01 17:15:51 +0200

    Suppression d'une colonne et ajout des animations

--- js/menu.js
+++ js/menu.js
@@ -2,15 +2,24 @@ $( document ).ready(function() {
   //quand on clique sur le logo
   $('#logo').on('click', function() {
     $('.acc').css('width', '100%');
+    //permet l'animation de retour
+    $('.titreAcc').removeClass('bounceOutUp');
+    $('.titreAcc').addClass('bounceInDown');
+    //on refixe les delay d'animation
+    $('#acc1 .titreAcc').css('animation-delay', '0s');
+    $('#acc2 .titreAcc').css('animation-delay', '.3s');
+    $('#acc3 .titreAcc').css('animation-delay', '.6s');
+    // $('.titreAcc').show();
   })
 
   //Quand on clique sur un des pan de l'accordeon
   $('.acc').on('click', function() {
-      $('.acc').css('width', '0%');
-      $('.acc')
-      // $('.acc').hide();
-      // $(this).show();
-      $(this).css('width', '100%');
+      $('.acc').css('width', '0%'); //On ferme tout
+      $(this).css('width', '100%'); //sauf celui qu'on selectionne
+      
+      $('.titreAcc').removeClass('bounceInDown');
+      $('.titreAcc').css('animation-delay', '1s');
+      $('.titreAcc').addClass('bounceOutUp');
     })
 
 });
