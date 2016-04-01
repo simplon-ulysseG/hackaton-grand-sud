2233e095b536d970ea235ac4a962b70764c74516
FlyingSocket <thomasmejovsek@hotmail.com>
2016-04-01 17:53:32 +0200

    ajout du responsive

--- js/menu.js
+++ js/menu.js
@@ -1,4 +1,35 @@
 $( document ).ready(function() {
+
+
+  //POUR LE RESPONSIVE
+  if ($(window).width() <= 600) {
+    //quand on clique sur le logo
+    $('#logo').on('click', function() {
+      $('.acc').css('height', '100%');
+      //permet l'animation de retour
+      $('.titreAcc').removeClass('bounceOutUp');
+      $('.titreAcc').addClass('bounceInDown');
+      //on refixe les delay d'animation
+      $('#acc1 .titreAcc').css('animation-delay', '0s');
+      $('#acc2 .titreAcc').css('animation-delay', '.3s');
+      $('#acc3 .titreAcc').css('animation-delay', '.6s');
+      // $('.titreAcc').show();
+    })
+
+    //Quand on clique sur un des pan de l'accordeon
+    $('.acc').on('click', function() {
+        $('.acc').css('height', '0%'); //On ferme tout
+        $(this).css('height', '100%'); //sauf celui qu'on selectionne
+
+        $('.titreAcc').removeClass('bounceInDown');
+        $('.titreAcc').css('animation-delay', '1s');
+        $('.titreAcc').addClass('bounceOutUp');
+      })
+
+  }
+
+
+  else {
   //quand on clique sur le logo
   $('#logo').on('click', function() {
     $('.acc').css('width', '100%');
@@ -16,10 +47,12 @@ $( document ).ready(function() {
   $('.acc').on('click', function() {
       $('.acc').css('width', '0%'); //On ferme tout
       $(this).css('width', '100%'); //sauf celui qu'on selectionne
-      
+
       $('.titreAcc').removeClass('bounceInDown');
       $('.titreAcc').css('animation-delay', '1s');
       $('.titreAcc').addClass('bounceOutUp');
     })
+
+    }
 
 });
